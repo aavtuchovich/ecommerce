87c1c233a308cf0d81a5c9d2072db602deb3c5a7
Andrey Avtuchovich <andrey.avtuchovich@gmail.com>
2018-03-16 15:11:13 +0300

    Initial project, added maven dependency and hello controller

---
+++ src/main/java/org/solteh/HelloController.java
@@ -0,0 +1,21 @@
+package org.solteh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class HelloController {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(HelloController.class, args);
+    }
+}
