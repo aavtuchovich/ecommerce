0e51ca793b34021166d57f7b10c379a25bd379cd
Andrey Avtuchovich <andrey.avtuchovich@gmail.com>
2018-03-20 17:04:14 +0300

    change configuration

--- src/main/java/org/solteh/WebSecurityConfig.java
+++ src/main/java/org/solteh/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/")
+        http.authorizeRequests().antMatchers("/","/topsales","/contacts","/products")
                 .permitAll().anyRequest().authenticated()
                 .and().formLogin()
                 .loginPage("/login").permitAll()
